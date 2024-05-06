e32bc37208b909be7fc6463a72a5fe788881254f
misharepbel <michael.tryfanau@gmail.com>
2024-05-06 18:18:18 +0200

    2024.05.06 MT

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import render_template
+
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return render_template("index.html.jinja")
+
+@app.route('/extract')
+def extract():
+    return render_template("extract.html.jinja")
+
+@app.route('/products')
+def products():
+    return render_template("products.html.jinja")
+
+
+@app.route('/author')
+def author():
+    return render_template("author.html.jinja")
+
+@app.route('/product/<product_id>')
+def hello(product_id):
+    return render_template("product.html.jinja", product_id=product_id)
