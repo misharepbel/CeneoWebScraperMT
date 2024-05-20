3023a1d75225ea0b7627ce7b8221259a1d3e70ab
misharepbel <michael.tryfanau@gmail.com>
2024-05-20 18:16:18 +0200

    2024.05.20 MT

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,12 @@
 from app import app
+import os
+import json
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 import requests
+from app import utils
+from bs4 import BeautifulSoup
 from flask import render_template, request, redirect, url_for
 
 @app.route('/')
@@ -14,13 +21,42 @@ def extract():
         url = f"https://ceneo.pl/{product_id}"
         response = requests.get(url)
         if response.status_code == requests.codes['ok']:
-            #proces ekstrakcji
-            return redirect(url_for('product', product_id=product_id))
+            page_dom = BeautifulSoup(response.text, "html.parser")
+            opinions_count = utils.extract_data(page_dom, "a.product-review__link > span")
+            if opinions_count:
+                #proces ekstrakcji
+                url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+                all_opinions = []
+                while(url):
+                    response = requests.get(url)
+                    page_dom = BeautifulSoup(response.text, "html.parser")
+                    opinions = page_dom.select("div.js_product-review")
+                    for opinion in opinions:
+                        single_opinion = {
+                            key : utils.extract_data(opinion, *value)
+                                for key, value in utils.selectors.items()
+                        }
+                        all_opinions.append(single_opinion)
+                    try:
+                        url = "https://www.ceneo.pl/" + utils.extract_data(page_dom, "a.pagination__next", "href", False)
+                    except (TypeError, AttributeError):
+                        url = None
+                    if not os.path.exists("app/data"):
+                        os.mkdir("app/data")
+                    if not os.path.exists("app/data/opinions"):
+                        os.mkdir("app/data/opinions")
+                    with open(f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jsonf:
+                        json.dump(all_opinions, jsonf, indent=4, ensure_ascii=False)
+                    opinions = pd.DataFrame.from_dict(all_opinions)
+                return redirect(url_for('product', product_id=product_id))
+            return render_template("extract.html.jinja", error="Dla produktu o podanym kodzie nie  ma opinii.")
+        return render_template("extract.html.jinja", error="Produkt o podanym kodzie nie istnieje.")
     return render_template("extract.html.jinja")
 
 @app.route('/products')
 def products():
-    return render_template("products.html.jinja")
+    products = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
+    return render_template("products.html.jinja", products=products)
 
 
 @app.route('/author')
