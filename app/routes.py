8f31071c57e632289767fadad40440bcf6abb737
misharepbel <michael.tryfanau@gmail.com>
2024-05-27 17:55:44 +0200

    2024.05.27 MT

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+import io
 import os
 import json
 from matplotlib import pyplot as plt
@@ -7,7 +8,7 @@ import pandas as pd
 import requests
 from app import utils
 from bs4 import BeautifulSoup
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, send_file, url_for
 
 @app.route('/')
 @app.route('/index')
@@ -28,6 +29,7 @@ def extract():
                 url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
                 all_opinions = []
                 while(url):
+                    product_name = utils.extract_data(page_dom, "h1")
                     response = requests.get(url)
                     page_dom = BeautifulSoup(response.text, "html.parser")
                     opinions = page_dom.select("div.js_product-review")
@@ -48,6 +50,21 @@ def extract():
                     with open(f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jsonf:
                         json.dump(all_opinions, jsonf, indent=4, ensure_ascii=False)
                     opinions = pd.DataFrame.from_dict(all_opinions)
+                    opinions.rating = opinions.rating.apply(lambda rate: rate.split("/")[0].replace(",", ".")).astype(float)
+                    product = {
+                        'product_id' : product_id,
+                        'product_name' : product_name,
+                        'opinions_count' : opinions.shape[0],
+                        'pros_count' : int(opinions.pros.astype(bool).sum()),
+                        'cons_count' : int(opinions.cons.astype(bool).sum()),
+                        'average_rating' : opinions.rating.mean(),
+                        'rating_distribution' : opinions.rating.value_counts().reindex(np.arange(0,5.3,0.5),fill_value=0.0).to_dict(),
+                        'recommendation_distribution' : opinions.recommendation.value_counts(dropna=False).reindex(["Polecam", "Nie polecam", None]).to_dict()
+                    }
+                    if not os.path.exists("app/data/products"):
+                        os.mkdir("app/data/products")
+                    with open(f"app/data/products/{product_id}.json", "w", encoding="UTF-8") as jsonf:
+                        json.dump(product, jsonf, indent=4, ensure_ascii=False)
                 return redirect(url_for('product', product_id=product_id))
             return render_template("extract.html.jinja", error="Dla produktu o podanym kodzie nie  ma opinii.")
         return render_template("extract.html.jinja", error="Produkt o podanym kodzie nie istnieje.")
@@ -55,7 +72,11 @@ def extract():
 
 @app.route('/products')
 def products():
-    products = [filename.split(".")[0] for filename in os.listdir("app/data/opinions")]
+    products_list = [filename.split(".")[0] for filename in os.listdir("app/data/products")]
+    products = []
+    for product_id in products_list:
+        with open(f"app/data/products/{product_id}.json", "r", encoding="UTF-8") as jsonf:
+            products.append(json.load(jsonf))
     return render_template("products.html.jinja", products=products)
 
 
@@ -66,3 +87,18 @@ def author():
 @app.route('/product/<product_id>')
 def product(product_id):
     return render_template("product.html.jinja", product_id=product_id)
+
+@app.route('/product/download_json/<product_id>')
+def download_json(product_id):
+    return send_file(f"data/opinions/{product_id}.json", "text/json", as_attachment = True)
+
+@app.route('/product/download_csv/<product_id>')
+def download_csv(product_id):
+    opinions  =  pd.read_json(f"app/data/opinions/{product_id}.json")
+    buffer = io.BytesIO(opinions.to_csv(sep="|", decimal=",", index=False).encode())
+    return send_file(buffer, "text/csv", as_attachment = True, download_name=f"{product_id}.csv")
+
+@app.route('/product/download_xlsx/<product_id>')
+def download_xlsx(product_id):
+    #return send_file(buffer, "text/xlsx", as_attachment = True, download_name=f"{product_id}.xlsx")
+    pass
